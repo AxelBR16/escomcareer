e8b5a94140493746c99da7f25a7663493a8706f9
AxelBR16 <117136772+AxelBR16@users.noreply.github.com>
2025-06-08 22:38:39 -0600

    Retroalimentacion

---
+++ src/main/java/com/example/demo/domain/entities/Retroalimentacion.java
@@ -0,0 +1,29 @@
+package com.example.demo.domain.entities;
+
+import com.example.demo.security.entities.Aspirante;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Retroalimentacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int general;       // experiencia general
+    private int experiencias;  // opinión sobre experiencias/proyectos
+    private int recomendacion; // si la recomendación fue útil
+    private int sistema;       // si recomendaría el sistema
+
+    private LocalDate fecha;
+
+    @OneToOne
+    @JoinColumn(name = "aspirante_id", unique = true)
+    @JsonBackReference
+    private Aspirante aspirante;
+}
